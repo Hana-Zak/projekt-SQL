d307f75f0e22ea16a3f3f853c578924f908fa709
Hana <h.zakovaa@gmail.com>
2025-09-10 16:31:55 +0200

    sekundární tabulka

---
+++ projekt-SQL/Scripts/Script3_secondary_table.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE t_hana_zakova_project_SQL_secondary_final AS
+SELECT
+    e.country,                     
+    c.continent,
+    e.year,
+    e.gdp,
+    e.gini,
+    e.population
+FROM economies e
+JOIN countries c ON c.country = e.country 
+JOIN (SELECT DISTINCT "year" FROM t_hana_zakova_project_SQL_primary_final) y
+     ON y.year = e.year
+WHERE c.continent = 'Europe'
+ORDER BY e.year, e.country;
+
+--kontrola
+SELECT * FROM t_hana_zakova_project_SQL_secondary_final;
