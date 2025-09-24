e900cba7dcc7ccf74aca8df1774d048795581b73
Hana <h.zakovaa@gmail.com>
2025-09-24 15:45:55 +0200

    Q2

--- projekt-SQL/Scripts/Script5_research_question_2.sql
+++ projekt-SQL/Scripts/Script5_research_question_2.sql
@@ -1,12 +1,29 @@
 -- Q2: Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+
 SELECT
-    category_name,
-    MIN("year") AS first_year,
-    MAX("year") AS last_year,
-    ROUND(MIN(units_affordable)) AS affordable_first_year,
-    ROUND(MAX(units_affordable)) AS affordable_last_year
-FROM t_hana_zakova_project_SQL_primary_final
-WHERE LOWER(category_name) LIKE '%mlék%'
-   OR LOWER(category_name) LIKE '%chléb%'
-GROUP BY category_name
-ORDER BY category_name;
+    fp.category_name,
+    MIN(fp.year) AS first_year,
+    MAX(fp.year) AS last_year,
+    ROUND(MIN(w.national_avg_wage / fp.avg_price), 0) AS affordable_first_year,
+    ROUND(MAX(w.national_avg_wage / fp.avg_price), 0) AS affordable_last_year
+FROM (
+    SELECT
+        year,
+        AVG(avg_monthly_wage_czk) AS national_avg_wage
+    FROM t_hana_zakova_project_SQL_primary_final
+    GROUP BY year
+) w
+JOIN (
+    SELECT
+        year,
+        category_name,
+        AVG(avg_price_czk) AS avg_price
+    FROM t_hana_zakova_project_SQL_primary_final
+    WHERE LOWER(category_name) LIKE '%mléko%'
+       OR LOWER(category_name) LIKE '%chléb%'
+    GROUP BY year, category_name
+) fp
+ON w.year = fp.year
+GROUP BY fp.category_name
+ORDER BY fp.category_name;
+
