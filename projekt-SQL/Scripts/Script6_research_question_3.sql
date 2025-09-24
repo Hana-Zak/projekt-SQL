95d5959748a67af7f7e6dd2d92de5d8ab1483550
Hana <h.zakovaa@gmail.com>
2025-09-24 15:53:43 +0200

    Q3

--- projekt-SQL/Scripts/Script6_research_question_3.sql
+++ projekt-SQL/Scripts/Script6_research_question_3.sql
@@ -1,24 +1,23 @@
 -- Q3: Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
-WITH price_changes AS (
-    SELECT
-        category_name,
-        "year",
-        avg_price_czk,
-        LAG(avg_price_czk) OVER (PARTITION BY category_name ORDER BY "year") AS prev_price
-    FROM t_hana_zakova_project_SQL_primary_final
-),
-price_growth AS (
+
+SELECT
+    category_name,
+    ROUND(AVG(yoy_growth_percent)::numeric, 2) AS avg_yoy_growth_percent
+FROM (
     SELECT
         category_name,
         "year",
         ROUND(((avg_price_czk - prev_price) / NULLIF(prev_price,0) * 100)::numeric, 2) AS yoy_growth_percent
-    FROM price_changes
+    FROM (
+        SELECT
+            category_name,
+            "year",
+            avg_price_czk,
+            LAG(avg_price_czk) OVER (PARTITION BY category_name ORDER BY "year") AS prev_price
+        FROM t_hana_zakova_project_SQL_primary_final
+    ) AS price_changes
     WHERE prev_price IS NOT NULL
-)
-SELECT
-    category_name,
-    ROUND(AVG(yoy_growth_percent)::numeric, 2) AS avg_yoy_growth_percent
-FROM price_growth
+) AS price_growth
 GROUP BY category_name
 ORDER BY avg_yoy_growth_percent ASC
 LIMIT 5;
